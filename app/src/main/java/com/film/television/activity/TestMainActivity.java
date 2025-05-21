6711e3472bbf4534e7301aa8b2f6e58874ebe3b1
zenglinggui <zenglinggui@pekon.com>
2025-05-21 10:24:24 +0800

    添加回调

--- app/src/main/java/com/film/television/activity/TestMainActivity.java
+++ app/src/main/java/com/film/television/activity/TestMainActivity.java
@@ -14,6 +14,7 @@ import androidx.core.content.ContextCompat;
 
 import com.common.wheel.admanager.AdvertisementManager;
 import com.common.wheel.admanager.InfoAdCallBack;
+import com.common.wheel.admanager.InformationFlowAdCallback;
 import com.common.wheel.admanager.OpenScreenAdCallBack;
 import com.common.wheel.admanager.RewardAdCallBack;
 import com.common.wheel.util.DeviceUtil;
@@ -59,28 +60,43 @@ public class TestMainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 AdvertisementManager.getInstance().showInterstitialAd(TestMainActivity.this, "102935580", new InfoAdCallBack() {
                     @Override
+                    public void onError() {
+                        Log.i("", "------插屏广告获取失败");
+                    }
+
+                    @Override
+                    public void onLoadSuccess() {
+                        Log.i("", "------插屏广告获取成功");
+                    }
+
+                    @Override
+                    public void onStartShow() {
+                        Log.i("", "------插屏广告开始显示");
+                    }
+
+                    @Override
                     public void onAdShow() {
-                        Log.i("","页面提示：插屏广告已展示");
+                        Log.i("", "------插屏广告已展示");
                     }
 
                     @Override
                     public void onAdVideoBarClick() {
-                        Log.i("","页面提示：插屏广告被点击");
+                        Log.i("", "------插屏广告被点击");
                     }
 
                     @Override
                     public void onAdClose() {
-                        Log.i("","页面提示：插屏广告被关闭");
+                        Log.i("", "------插屏广告被关闭");
                     }
 
                     @Override
                     public void onVideoComplete() {
-
+                        Log.i("", "------插屏广告视频完成");
                     }
 
                     @Override
                     public void onSkippedVideo() {
-
+                        Log.i("", "------插屏广告视频跳过");
                     }
                 });
             }
@@ -94,8 +110,28 @@ public class TestMainActivity extends AppCompatActivity {
                 AdvertisementManager.getInstance().showOpenScreenAd(TestMainActivity.this, "102936358", splashContainer, 1000, 1920, new OpenScreenAdCallBack() {
                     @Override
                     public void onAdClose() {
-//                        LogUtils.i("广告关闭");
+                        Log.i("", "------开屏广告关闭");
                         splashContainer.removeAllViews();
+                    }
+
+                    @Override
+                    public void onSplashAdClick() {
+                        Log.i("", "------开屏广告被点击");
+                    }
+
+                    @Override
+                    public void onSplashAdShow() {
+                        Log.i("", "------开屏广告已显示");
+                    }
+
+                    @Override
+                    public void onSplashLoadFail() {
+                        Log.i("", "------开屏广告获取失败");
+                    }
+
+                    @Override
+                    public void onSplashRenderFail() {
+                        Log.i("", "------开屏广告渲染失败");
                     }
                 });
             }
@@ -103,13 +139,38 @@ public class TestMainActivity extends AppCompatActivity {
         findViewById(R.id.show_info_image_ad).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AdvertisementManager.getInstance().showInfoFlowAd(TestMainActivity.this, "102934170", infoContainer, 800, 400);
+                AdvertisementManager.getInstance().showInfoFlowAd(TestMainActivity.this, "102934170", infoContainer, 800, 400, null);
             }
         });
         findViewById(R.id.show_info_image_ad2).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AdvertisementManager.getInstance().showInfoFlowAd(TestMainActivity.this, "102934170", infoContainer2, 800, 400);
+                AdvertisementManager.getInstance().showInfoFlowAd(TestMainActivity.this, "102934170", infoContainer2, 900, 300, new InformationFlowAdCallback() {
+                    @Override
+                    public void onError() {
+                        Log.i("", "------信息流广告获取失败");
+                    }
+
+                    @Override
+                    public void onFeedAdLoad() {
+                        Log.i("", "------信息流广告获取成功");
+                    }
+
+                    @Override
+                    public void onRenderSuccess() {
+                        Log.i("", "------信息流广告渲染成功");
+                    }
+
+                    @Override
+                    public void onAdClick() {
+                        Log.i("", "------信息流广告被点击");
+                    }
+
+                    @Override
+                    public void onRenderFail() {
+                        Log.i("", "------信息流广告显示失败");
+                    }
+                });
             }
         });
         findViewById(R.id.http_request).setOnClickListener(new View.OnClickListener() {
@@ -118,17 +179,42 @@ public class TestMainActivity extends AppCompatActivity {
                 AdvertisementManager.getInstance().showRewardAd(TestMainActivity.this, "102935581", new RewardAdCallBack() {
                     @Override
                     public void onAdClose() {
-
+                        Log.i("", "------激励广告关闭");
                     }
 
                     @Override
                     public void onVideoComplete() {
-
+                        Log.i("", "------激励广告视频完成");
                     }
 
                     @Override
                     public void onAdVideoBarClick() {
+                        Log.i("", "------激励广告视频被点击");
+                    }
 
+                    @Override
+                    public void onVideoError() {
+                        Log.i("", "------激励广告视频获取失败");
+                    }
+
+                    @Override
+                    public void onRewardArrived() {
+                        Log.i("", "------激励广告激励发放");
+                    }
+
+                    @Override
+                    public void onSkippedVideo() {
+                        Log.i("", "------激励广告跳过");
+                    }
+
+                    @Override
+                    public void onAdShow() {
+                        Log.i("", "------激励广告已显示");
+                    }
+
+                    @Override
+                    public void onError() {
+                        Log.i("", "------激励广告加载失败");
                     }
                 });
             }
