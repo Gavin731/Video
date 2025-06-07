a11548f6ff32430c26213befe584a8a2e4fa75b3
zenglinggui <zenglinggui@pekon.com>
2025-06-07 20:23:59 +0800

    添加oaid

--- app/src/main/java/com/film/television/MyApplication.kt
+++ app/src/main/java/com/film/television/MyApplication.kt
@@ -4,6 +4,8 @@ import android.app.Application
 import android.util.Log
 import androidx.lifecycle.ProcessLifecycleOwner
 import androidx.lifecycle.lifecycleScope
+import com.bytedance.sdk.openadsdk.TTCustomController
+import com.bytedance.sdk.openadsdk.mediation.init.MediationPrivacyConfig
 import com.common.wheel.admanager.AdvertisementManager
 import com.common.wheel.admanager.InitCallback
 import com.film.television.utils.DataStoreUtil
@@ -37,8 +39,44 @@ class MyApplication : Application() {
                 override fun error() {
                     Log.i("","初始化失败回调")
                 }
-            });
-        AdvertisementManager.getInstance().initConfig();
+            }, getTTCustomController());
+        AdvertisementManager.getInstance().initConfig("TEST0000001");
+    }
+
+    private fun getTTCustomController(): TTCustomController {
+        return object : TTCustomController() {
+            override fun isCanUseLocation(): Boolean {  //是否授权位置权限
+                return true
+            }
+
+            override fun isCanUsePhoneState(): Boolean {  //是否授权手机信息权限
+                return true
+            }
+
+            override fun isCanUseWifiState(): Boolean {  //是否授权wifi state权限
+                return true
+            }
+
+            override fun isCanUseWriteExternal(): Boolean {  //是否授权写外部存储权限
+                return true
+            }
+
+            override fun isCanUseAndroidId(): Boolean {  //是否授权Android Id权限
+                return true
+            }
+
+            override fun getMediationPrivacyConfig(): MediationPrivacyConfig? {
+                return object : MediationPrivacyConfig() {
+                    override fun isLimitPersonalAds(): Boolean {  //是否限制个性化广告
+                        return false
+                    }
+
+                    override fun isProgrammaticRecommend(): Boolean {  //是否开启程序化广告推荐
+                        return true
+                    }
+                }
+            }
+        }
     }
 
     companion object {
